2941f4a9549d62452655f6cbbdce86d95841487b
李啸天 <lixiaotian@you-fuli.com>
2019-04-01 18:22:13 +0800

    feat(*):读取excel文件并修改值，然后删除

--- src/main/java/com/selenium/utils/POIUtil.java
+++ src/main/java/com/selenium/utils/POIUtil.java
@@ -1,16 +1,14 @@
 package com.selenium.utils;
 
-import com.opencsv.CSVReader;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 /**
  * Created by 李啸天 on 2018/10/9.
@@ -23,6 +21,7 @@ public class POIUtil {
 
     /**
      * 读取文件，并修改excel文件中的优分金额
+     *
      * @param file
      */
     public static void readExcel(File file) {
@@ -37,32 +36,89 @@ public class POIUtil {
             //表格总计多少行
             int total = sheet.getLastRowNum();
             //遍历每一行
-            for(Row row : sheet){
+            for (Row row : sheet) {
                 //获取第1，2个单元格对象
                 //如果cell为空，直接用row.createCell(0)就行
                 Cell cell1 = row.getCell(2);
                 cell1.setCellType(CellType.STRING);
                 String cellValue = cell1.getStringCellValue();
                 System.out.println(cellValue);
-                if("0".equals(cellValue)){
+                if ("0".equals(cellValue)) {
                     cell1.removeCellComment();
                     cell1.setCellValue("100");
                 }
             }
             //对文档的写入还在缓存区，需要把流刷到文件中
-            FileOutputStream out=new FileOutputStream(file);
+            FileOutputStream out = new FileOutputStream(file);
             workbook2.write(out);
             //该关闭的关闭
             workbook2.close();
             out.close();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    public static String FixFileName(String filePath, String newFileName) {
+        File f = new File(filePath);
+        if (!f.exists()) { // 判断原文件是否存在（防止文件名冲突）
+            return null;
+        }
+        newFileName = newFileName.trim();
+        if ("".equals(newFileName) || newFileName == null) // 文件名不能为空
+            return null;
+        String newFilePath = null;
+        if (f.isDirectory()) { // 判断是否为文件夹
+            newFilePath = filePath.substring(0, filePath.lastIndexOf("\\")) + "\\" + newFileName;
+        } else {
+            newFilePath = filePath.substring(0, filePath.lastIndexOf("\\")) + "\\" + newFileName;
+        }
+        File nf = new File(newFilePath);
+        try {
+            f.renameTo(nf); // 修改文件名
+        } catch (Exception err) {
+            err.printStackTrace();
+            return null;
+        }
+        return newFilePath;
+    }
 
-    public static void main(String[] args) {
-        File file = new File("E:\\2019\\20190401\\20190401154424570182.xls");
+    /**
+     * 读取某个文件夹下面的xls结尾的文件
+     * @param fileUrl
+     * @return
+     */
+    public static String xlsUrl(String fileUrl){
+        try {
+            File file = ResourceUtils.getFile(fileUrl);
+            File [] files = file.listFiles();
+            for(File name:files){
+                String names = name.toString();
+                if(names.endsWith(".xls")){
+                    System.out.println(names);
+                    return names;
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        //定义文件夹
+        String path = "E:\\2019\\20190401";
+        //查询文件夹下以xls结尾的文件
+        String fileName = POIUtil.xlsUrl(path);
+        //读取
+        File file  = ResourceUtils.getFile(fileName);
+        //修改优分余额
         POIUtil.readExcel(file);
+
+        //上传操作完成后删除
+        file.delete();
+        //POIUtil.FixFileName(fileName,"123.xls");
+
     }
 }
